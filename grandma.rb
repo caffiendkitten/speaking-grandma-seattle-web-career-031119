d789686cbaf92850152846dfaa5751aeb5fd693e
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-03-02 18:56:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(say)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif (say != say.downcase)
     return  "NO, NOT SINCE 1938!"
-  elsif (say == say.downcase)
+  elsif (say != say.upcase)
     return "HUH?! SPEAK UP, SONNY!"
   end
 
