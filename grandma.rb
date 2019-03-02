85da5c065e44c4be38b6072f16663bde390e67f6
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-03-02 19:01:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(say)
   if (say == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif (say != say.downcase)
+  elsif (say == say.upcase)
     return  "NO, NOT SINCE 1938!"
   elsif (say != say.upcase)
     puts "HUH?! SPEAK UP, SONNY!"
